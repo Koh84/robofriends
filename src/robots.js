1aad652df9eeef1f12363ce07f150d40e78bd2b0
Kelvin <koh84@hotmail.com>
2021-12-12 17:30:41 +0000

    Robofriends webpage

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'alibaba',
+        username: 'Bret',
+        email: 'abc@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'nio',
+        username: 'Bret 2',
+        email: 'abc@gmail.com 2'
+    },
+    {
+        id: 3,
+        name: 'xpeng',
+        username: 'Bret 3',
+        email: 'abc@gmail.com 3'
+    },
+    {
+        id: 4,
+        name: 'xiaomi',
+        username: 'Bret 4',
+        email: 'abc@gmail.com 4'
+    },
+    {
+        id: 5,
+        name: 'arrival',
+        username: 'Bret 5',
+        email: 'abc@gmail.com 5'
+    },
+    {
+        id: 6,
+        name: 'hyliion',
+        username: 'Bret 6',
+        email: 'abc@gmail.com 6'
+    }
+]
